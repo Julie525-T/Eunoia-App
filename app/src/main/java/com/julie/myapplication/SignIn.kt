aa2bf69af9690b0645f830396f1f323115576a02
Julie525-T <jullytracy576@gmail.com>
2026-04-13 16:29:16 +0300

    I have consumed the APIs I used in the sokogarden backend in this project.

--- app/src/main/java/com/julie/myapplication/SignIn.kt
+++ app/src/main/java/com/julie/myapplication/SignIn.kt
@@ -1,10 +1,15 @@
 package com.julie.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.loopj.android.http.RequestParams
 
 class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,41 @@ class SignIn : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+//            https://julie.alwaysdata.net/api/addproducts
+//            https://julie.alwaysdata.net/api/signin
+//            https://julie.alwaysdata.net/api/signup
+//            https://julie.alwaysdata.net/api/mpesa_payment
+//            https://julie.alwaysdata.net/api/getproductdetails
+
+        }
+
+        val signin=findViewById<TextView>(R.id.signin_link)
+
+        signin.setOnClickListener{
+            val signInlink= Intent(applicationContext,SignUp::class.java)
+            startActivity(signInlink)
+        }
+
+        val email=findViewById<EditText>(R.id.email)
+
+        val password=findViewById<EditText>(R.id.password)
+
+        val signIn=findViewById<Button>(R.id.signin)
+
+        signIn.setOnClickListener {
+            val api="https://julie.alwaysdata.net/api/signin"
+
+//            RequestParams is the container used to collect the user details. It is like form data in js
+            val data= RequestParams()
+
+            data.put("email",email.text.toString().trim())
+            data.put("password",password.text.toString().trim())
+
+//            API helper-it deli8vers our data to the api
+
+            val helper= ApiHelper(applicationContext)
+            helper.post_login(api,data)
         }
     }
 }
